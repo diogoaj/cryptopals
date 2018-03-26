e19d695be3568480394c7088bbf525318c5b78b1
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2018-03-26 15:37:33 +0100

    Add solution to challenge22

---
+++ set3/challenge22/solve.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.insert(0, '../')
+
+""" Crack an MT19937 seed """
+
+from challenge21.solve import MarsenneTwisterMT19937
+import random
+import time
+
+# Generate output
+time.sleep(random.randint(40, 1000))
+rng = MarsenneTwisterMT19937()
+time.sleep(random.randint(40, 1000))
+
+output = rng.generateNumber()
+print("Output " + str(output))
+
+# Crack Seed
+t = int(time.time())
+
+while True:
+	rng = MarsenneTwisterMT19937(t)
+	if rng.generateNumber() == output:
+		print ("Found seed: " + str(t))
+		break
+	t -= 1
+
+print("Checking output...")
+print(output == MarsenneTwisterMT19937(t).generateNumber())
